688e62dbe91320ca9c7d0bd575c5d4f506e30506
T. Korn <tobias.korn@towerswatson.com>
2025-05-28 09:23:52 +0200

    Buildable and green Tests

--- dotnet/test/KornSW.DynamicProxy.Tests.net48/Properties/AssemblyInfo.cs
+++ dotnet/test/KornSW.DynamicProxy.Tests.net48/Properties/AssemblyInfo.cs
@@ -15,6 +15,6 @@ using System.Runtime.InteropServices;
 
 [assembly: Guid("d0b8734c-340f-4b2a-80ee-c00afdd59e13")]
 
-[assembly: AssemblyVersion("0.1.0.*")]
-[assembly: AssemblyInformationalVersion("1.1.0-localbuild")]
+[assembly: AssemblyVersion("999.0.0.*")]
+[assembly: AssemblyInformationalVersion("999.0.0-localbuild")]
 //DONT ADD: [assembly: AssemblyFileVersion] !!!
